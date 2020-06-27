0aba1873e9c05216e1aae06f87b55bfad0c6bea2
Alex Silva Figueroa <alexbastiansilvafigueroa@gmail.com>
2020-06-26 20:11:50 -0400

    Se agregan todos los archivos al respositorio

---
+++ api/index.js
@@ -0,0 +1,17 @@
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const Users = mongoose.model('User', new mongoose.Schema({ name: String}))
+Users.create({ name: 'Curso JS'})
+
+
+app.get('*', (req, res) => {
+    Users.find()
+    .then(x => res.send(x))
+})
+
+module.exports = app
